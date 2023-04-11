dd162b2907320a3cf6116a3c2553451cfcfa6f97
Faisal Richard Jr <faisalrichardjr@gmail.com>
2023-04-11 09:56:49 +0800

    made some changes to sirSim

--- sirSimulation.py
+++ sirSimulation.py
@@ -2,6 +2,24 @@ import math
 
 def compute_for_values(s0, i0, a, c, sigma, t):
 
+    # Warn the user if they do not provide a number as their inputs
+    try:
+        s0 = int(s0)
+        i0 = int(i0)
+        a = float(s0)
+        c = float(c0)
+        sigma = float(sigma)
+        t = float(t)
+    except ValueError:
+        return "Error: input must all be numbers"
+
+    # Make sure that S(0) and I(0) is greater than 0
+    if (s0 <= 0):
+        return "Error: The number of susceptible individuals must be greater than 0"
+
+    if (i0 <= 0):
+        return "Error: The number of infected individuals must be greater than 0"
+
     # Calculate population at time t
     N = S0 + I0
     r = a * c
